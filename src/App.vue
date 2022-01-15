3809ca58855afe62c75e620a63dec9b1dbdb55f1
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-15 23:32:09 +0200

    feat: added posts logic

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,38 @@
 <template>
   Hello work
-  <div>
-    <div class="post">
-      <div>
-        <strong>Title:</strong> Post about JS
-      </div>
-      <div>
-        <strong>Description:</strong> JS universal language
-      </div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4></h4>
+      <input @input="title=$event.target.value"
+             v-bind:value="title" class="input"
+             type="text" placeholder="title">
+      <input @input="body=$event.target.value"
+             v-bind:value="body"
+             class="input" type="text"
+             placeholder="description">
+      <button v-on:click="createPost" class="button">Create post</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Title:</strong>{{ post.title }}</div>
+      <div><strong>Description:</strong>{{ post.body }}</div>
     </div>
-    <div class="post">
-      <div>
-        <strong>Title:</strong> Post about JS
-      </div>
-      <div>
-        <strong>Description:</strong> JS universal language
-      </div>
-    </div>
-    <div class="post">
-      <div>
-        <strong>Title:</strong> Post about JS
-      </div>
-      <div>
-        <strong>Description:</strong> JS universal language
-      </div>
-    </div>
-
-
   </div>
 </template>
 <script>
 export default {
   name: 'App',
+  //models
   data() {
     return {
       likes: 0,
       disLikes: 5,
-      posts:[
-        {id:1,title:'JS1',body:'description 1'},
-        {id:2,title:'JS2',body:'description 2'},
-        {id:3,title:'JS3',body:'description 3'},
-      ]
+      posts: [
+        {id: 1, title: 'JS1', body: 'description 1'},
+        {id: 2, title: 'JS2', body: 'description 2'},
+        {id: 3, title: 'JS3', body: 'description 3'},
+      ],
+      title: '',
+      body: ''
     }
   },
   methods: {
@@ -49,7 +41,23 @@ export default {
     },
     addDisLike() {
       this.disLikes += 1;
-    }
+    },
+    createPost() {
+      // e.stopPropagation();
+      // e.preventDefault();
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+
+      }
+      this.posts.push(newPost)
+      this.title='';
+      this.body=''
+
+    },
+
+
   }
 
 
@@ -70,4 +78,23 @@ export default {
   border: 2px solid green;
 }
 
+.input {
+  width: 300px;
+  border: 2px solid gray;
+  margin: 20px;
+  padding: 10px;
+  font-size: 20px;
+
+}
+
+.app {
+  margin: 20px;
+}
+
+.button {
+  margin: 20px;
+  padding: 10px;
+  font-size: 20px;
+  cursor: pointer;
+}
 </style>
