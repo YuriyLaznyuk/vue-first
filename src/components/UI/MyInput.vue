fdaaa96a0407f41bc73c755ff088d368d3b24ede
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-19 19:01:32 +0200

    feat: added search

--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,39 +1,50 @@
 <template>
+  <div class="title" v-if="title">{{ title }}</div>
   <input
-      :value="modelValue"
-      @input="updateInput"
-      class="input" type="text">
+    :value="modelValue"
+    @input="updateInput"
+    class="input"
+    type="text"
+    :placeholder="placeholder"
+  />
 
-<!--  <input-->
-<!--      :value="value"-->
-<!--      @input="updateInput"-->
-<!--      class="input" type="text">-->
-
+  <!--  <input-->
+  <!--      :value="value"-->
+  <!--      @input="updateInput"-->
+  <!--      class="input" type="text">-->
 </template>
 
 <script>
 export default {
   name: "MyInput",
   props: {
-    modelValue: [String, Number]
+    modelValue: [String, Number],
     // value: [String, Number]
+    title: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
   },
   methods: {
     updateInput(event) {
-      this.$emit('update:modelValue', event.target.value)
+      this.$emit("update:modelValue", event.target.value);
       // this.$emit('update:value', event.target.value)
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
 .input {
   width: 300px;
   border: 2px solid gray;
-  margin: 20px;
+  margin: 5px 20px 20px 20px;
   padding: 10px;
   font-size: 20px;
-
+}
+.title {
+  margin: 20px 20px 0 20px;
 }
 </style>
