b4f4138f97be41cd74859c6d1fa683263cc9135d
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-20 00:06:44 +0200

    feat: added router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import components from './components/UI'
-const app=createApp(App)
+import { createApp } from "vue";
+import App from "./App.vue";
+import components from "./components/UI";
+import router from "./router/router";
+const app = createApp(App);
 // createApp(App).mount('#app')
 
-components.forEach(component=>app.component(component.name,component))
-app.mount('#app')
+components.forEach((component) => app.component(component.name, component));
+app.use(router).mount("#app");
---
+++ src/pages/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>MAIN</h1>
+    <router-link to="/">Main</router-link>
+    <router-link to="/posts">Posts</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style scoped></style>
