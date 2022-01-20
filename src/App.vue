53c1b467df2b377b1873f52b3433d08167525dd8
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-20 16:37:05 +0200

    feat: added nav btn push

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
+  <Navbar></Navbar>
   <div class="app">
     <router-view> </router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "App",
+  components: { Navbar },
 };
 </script>
 
-<style scoped>
+<style>
 * {
   margin: 0;
   padding: 0;
